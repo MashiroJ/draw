38a23562b2916cf3eb1022b00e85c78458dd8519
Mashiro <1146446176@qq.com>
2024-09-14 12:45:53 +0800

    更改项目结构

--- web/src/main/java/com/mashiro/draw/web/DrawApplication.java
+++ web/src/main/java/com/mashiro/draw/web/DrawApplication.java
@@ -1,8 +1,9 @@
-package com.mashiro.web;
+package com.mashiro.draw.web;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@MapperScan("com.mashiro.draw.mapper")
 @SpringBootApplication
 public class DrawApplication {
 
