9e837feaad53ed8480651a663a7900e544ac6ca8
Mashiro <1146446176@qq.com>
2024-09-14 23:54:06 +0800

    项目结构优化

--- web/src/main/java/com/mashiro/DrawApplication.java
+++ web/src/main/java/com/mashiro/DrawApplication.java
@@ -1,9 +1,9 @@
-package com.mashiro.draw.web;
+package com.mashiro;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//@MapperScan("com.mashiro.draw.mapper")
+@MapperScan("com.mashiro.mapper")
 @SpringBootApplication
 public class DrawApplication {
 
