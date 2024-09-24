650aa2794872bfefb070bb905593f23f2d8749c0
Mashiro <1146446176@qq.com>
2024-09-24 22:56:06 +0800

    文档：添加Knife4j配置以改进API文档
    添加Knife4jConfiguration类，提供openAPI()和userAPI()、systemAPI()等方法，以增强API文档和分组管理。

---
+++ common/src/main/java/com/mashiro/utils/Knife4jConfiguration/Knife4jConfiguration.java
@@ -0,0 +1,34 @@
+package com.mashiro.utils.Knife4jConfiguration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Knife4jConfiguration {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Draw项目API")
+                        .version("1.0")
+                        .description("Draw项目的接口文档"));
+    }
+    
+    @Bean
+    public GroupedOpenApi userAPI() {
+        return GroupedOpenApi.builder().group("用户信息管理").
+                pathsToMatch("/system/user/**").
+                build();
+    }
+
+    @Bean
+    public GroupedOpenApi systemAPI() {
+        return GroupedOpenApi.builder().group("角色信息管理").
+                pathsToMatch("/system/role/**").
+                build();
+    }
+}
--- common/src/main/java/com/mashiro/utils/mybatisplus/MybatisPlusConfig.java
+++ common/src/main/java/com/mashiro/utils/mybatisplus/MybatisPlusConfig.java
@@ -12,7 +12,6 @@ import org.springframework.context.annotation.Configuration;
  * MybatisPlus-分页配置类
  */
 @Configuration
-@MapperScan("com.mashiro.mapper")
 public class MybatisPlusConfig {
 
     /**
