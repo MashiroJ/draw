6d592df8eabc8890da97a4e22a7b85caa1c83731
Mashiro <1146446176@qq.com>
2024-09-26 22:06:12 +0800

    ```markdownfeat(minio):集成MinIO对象存储服务
    应用程序现在支持MinIO对象存储服务，通过新增的配置属性和MinIO客户端Bean实现文件存储和检索功能。
    ```

---
+++ common/src/main/java/com/mashiro/utils/minio/MinioConfiguration.java
@@ -0,0 +1,27 @@
+package com.mashiro.utils.minio;
+
+import io.minio.MinioClient;
+import jakarta.annotation.Resource;
+import lombok.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnProperty(name = "minio.endpoint") // 如果配置了minio.endpoint，则启用该配置类
+@EnableConfigurationProperties(MinioProperties.class)
+public class MinioConfiguration {
+
+    @Resource
+    private MinioProperties minioProperties;
+
+    @Bean
+    public MinioClient minioClient() {
+        return MinioClient.builder()
+                .endpoint(minioProperties.getEndpoint())
+                .credentials(minioProperties.getAccessKey(), minioProperties.getSecretKey())
+                .build();
+    }
+
+}
---
+++ common/src/main/java/com/mashiro/utils/minio/MinioProperties.java
@@ -0,0 +1,13 @@
+package com.mashiro.utils.minio;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+@Data
+@ConfigurationProperties(prefix = "minio")
+public class MinioProperties {
+    private String endpoint;
+    private String accessKey;
+    private String secretKey;
+    private String bucketName;
+}
