0ba0ea45d9a9d70a93230cca6a49601da63a9a59
Mashiro <1146446176@qq.com>
2024-09-29 11:39:23 +0800

    数据库模式和映射更新：引入系统菜单表
    引入了一个新的系统菜单表，包含创建相应的实体类（Menu.java）、Mapper接口（MenuMapper.java）、Mapper XML文件（MenuMapper.xml）以及Service接口和实现（MenuService.java和MenuServiceImpl.java）。这将为系统提供基础的菜单管理功能。

---
+++ model/src/main/java/com/mashiro/entity/Menu.java
@@ -0,0 +1,81 @@
+package com.mashiro.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import java.io.Serializable;
+import java.util.Date;
+import lombok.Data;
+
+/**
+ * 系统菜单表
+ * @TableName sys_menu
+ */
+@TableName(value ="sys_menu")
+@Data
+public class Menu implements Serializable {
+    /**
+     * 菜单ID
+     */
+    @TableId(type = IdType.AUTO)
+    private Integer id;
+
+    /**
+     * 菜单名称
+     */
+    private String name;
+
+    /**
+     * 菜单路径
+     */
+    private String path;
+
+    /**
+     * 父菜单ID，根菜单为NULL
+     */
+    private Integer parentId;
+
+    /**
+     * 菜单图标
+     */
+    private String icon;
+
+    /**
+     * 排序号
+     */
+    private Integer sortOrder;
+
+    /**
+     * 权限标识
+     */
+    private String permission;
+
+    /**
+     * 菜单状态：1正常，0禁用
+     */
+    private Integer status;
+
+    /**
+     * 是否可见：1可见，0不可见
+     */
+    private Integer isVisible;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
+
+    /**
+     * 是否删除：0未删除，1已删除
+     */
+    private Integer isDeleted;
+
+    @TableField(exist = false)
+    private static final long serialVersionUID = 1L;
+}
---
+++ web/src/main/java/com/mashiro/service/MenuService.java
@@ -0,0 +1,13 @@
+package com.mashiro.service;
+
+import com.mashiro.entity.Menu;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author mashiro
+* @description 针对表【sys_menu(系统菜单表)】的数据库操作Service
+* @createDate 2024-09-29 11:38:42
+*/
+public interface MenuService extends IService<Menu> {
+
+}
