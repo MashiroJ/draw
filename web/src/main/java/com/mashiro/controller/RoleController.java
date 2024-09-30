0a64a0dc2b9133f86ef728af8808d5849c2cf170
Mashiro <1146446176@qq.com>
2024-09-30 16:22:51 +0800

    新增角色管理相关API接口
    - 添加RoleController类，实现角色管理功能
    - 实现了获取角色列表、保存或更新角色、删除角色的API接口
    - 使用Swagger注解对API进行文档化- 通过@Resource注解注入RoleService依赖

---
+++ web/src/main/java/com/mashiro/controller/RoleController.java
@@ -0,0 +1,41 @@
+package com.mashiro.controller;
+
+import com.mashiro.entity.Role;
+import com.mashiro.result.Result;
+import com.mashiro.service.RoleService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Tag(name = "角色管理")
+@RestController
+@RequestMapping("/system/role/")
+public class RoleController {
+
+    @Resource
+    private RoleService roleService;
+
+    @Operation(summary = "获取角色列表")
+    @GetMapping("list")
+    public Result listRole() {
+        List<Role> list = roleService.list();
+        return Result.ok(list);
+    }
+
+    @Operation(summary = "保存或更新角色")
+    @PostMapping("saveOrUpdate")
+    public Result saveRole(Role role) {
+        roleService.saveOrUpdate(role);
+        return Result.ok();
+    }
+
+    @Operation(summary = "删除角色")
+    @DeleteMapping("removeById")
+    public Result removeRole(@RequestParam Long id) {
+        roleService.removeById(id);
+        return Result.ok();
+    }
+}
