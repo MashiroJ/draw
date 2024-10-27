37a99ab30f388b664fc3f1ec9703d50930506739
Mashiro <1146446176@qq.com>
2024-10-27 09:18:01 +0800

    refactor(web): 注释掉 CorsConfig 类
    注释掉了 CorsConfig 类，该类用于配置跨域请求。目前有其他方式处理跨域问题。

--- web/src/main/java/com/mashiro/config/cors/CorsConfig.java
+++ web/src/main/java/com/mashiro/config/cors/CorsConfig.java
@@ -1,22 +1,22 @@
-package com.mashiro.config.cors;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
- public class CorsConfig implements WebMvcConfigurer {
- 
-     @Override
-     public void addCorsMappings(CorsRegistry registry) {
-         //支持所有接口
-         registry.addMapping("/**")
-                 //是否发送Cookie
-                .allowCredentials(true)
-                 //支持域
-                .allowedOriginPatterns("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*")
-                .exposedHeaders("*");
-    }
- }
+//package com.mashiro.config.cors;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+// public class CorsConfig implements WebMvcConfigurer {
+//
+//     @Override
+//     public void addCorsMappings(CorsRegistry registry) {
+//         //支持所有接口
+//         registry.addMapping("/**")
+//                 //是否发送Cookie
+//                .allowCredentials(true)
+//                 //支持域
+//                .allowedOriginPatterns("*")
+//                .allowedMethods("GET", "POST", "PUT", "DELETE")
+//                .allowedHeaders("*")
+//                .exposedHeaders("*");
+//    }
+// }
