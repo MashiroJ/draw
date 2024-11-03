e23cdce3e67432b9b9184a350d58484cee1696c8
Mashiro <1146446176@qq.com>
2024-11-03 22:19:00 +0800

    refactor(web): 删除图像生成相关代码
    - 移除 ImageController、ImageService 和 ImageServiceImpl 类- 删除与图像生成相关的所有接口和实现

--- web/src/main/java/com/mashiro/service/ImageService.java
+++
@@ -1,7 +0,0 @@
-package com.mashiro.service;
-
-import java.util.Map;
-
-public interface ImageService {
-    Map<String, Map<String, Object>> generateImages(Map<String, Object> prompt) throws Exception;
-}
