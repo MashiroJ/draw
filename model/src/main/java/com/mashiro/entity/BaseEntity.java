400e82405b6159d2a80b86707868ce38c2f75d5c
Mashiro <1146446176@qq.com>
2024-11-22 00:05:45 +0800

    refactor(model): 将 createTime 和 updateTime 字段类型改为 LocalDateTime
    - 修改 createTime 和 updateTime 字段的类型从 Date 改为 LocalDateTime
    - 更新相关注解和格式化配置
    - 优化代码结构，提高日期时间处理的灵活性和准确性

--- model/src/main/java/com/mashiro/entity/BaseEntity.java
+++ model/src/main/java/com/mashiro/entity/BaseEntity.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Data
@@ -19,17 +20,16 @@ public class BaseEntity implements Serializable {
     @Schema(description = "创建时间")
     @TableField(value = "create_time", fill = FieldFill.INSERT)
     @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
-    private Date createTime;
+    private LocalDateTime createTime;
 
     @Schema(description = "更新时间")
     @TableField(value = "update_time", fill = FieldFill.UPDATE)
     @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
-    private Date updateTime;
+    private LocalDateTime updateTime;
 
     @Schema(description = "逻辑删除")
     @TableField("is_deleted") // 指定数据库字段名
     @JsonIgnore // 忽略字段
     @TableLogic // TableLogic注解可以自动为查询操作增加`is_deleted=0`过滤条件，并将删除操作转为更新语句。
     private Byte isDeleted;
-
 }
